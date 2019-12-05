2f3d2793650702e572e68209d364d4e8b7e588d4
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-05 13:00:52 +0300

    Create The_default_keyword.js

---
+++ The_default_keyword.js
@@ -0,0 +1,17 @@
+var color ="yellow";
+switch(color) {
+    case "blue": 
+        document.write("This is blue.");
+        break;
+    case "red":     
+        document.write("This is red.");
+        break;
+    case "green": 
+        document.write("This is green.");    
+        break;
+    case "orange":  
+        document.write("This is orange.");    
+        break;
+    default: 
+        document.write("Color not found.");
+}
