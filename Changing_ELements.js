b709b0b21326d1c2963516cdbd9243174439bcd4
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-08 00:31:57 +0300

    Create Changing_ELements.js

---
+++ Changing_ELements.js
@@ -0,0 +1,5 @@
+//calling the function in window.onload to make sure the HTML is loaded
+window.onload = function() {
+    var el = document.getElementsByTagName('a');
+    el[0].href= 'http://www.sololearn.com';
+};
