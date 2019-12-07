9f20669520f86da07c71d2a1afff7a303e36a7eb
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-08 00:54:57 +0300

    Create Changing_Style.js
    calling the function in window.onload to make sure the HTML is loaded

---
+++ Changing_Style.js
@@ -0,0 +1,6 @@
+//calling the function in window.onload to make sure the HTML is loaded
+window.onload = function() {
+    var x = document.getElementById("demo");
+    x.style.color = '#6600FF';
+    x.style.width = '100px';
+};
