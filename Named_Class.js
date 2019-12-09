aa81c7428f3ea013cc95b7950832827c4d168c7b
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-09 20:01:04 +0300

    Create Named_Class.js

---
+++ Named_Class.js
@@ -0,0 +1,11 @@
+var Square = class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+  }
+};
+
+const square = new Square(5, 5);
+const poster = new Square(2, 3); 
+
+console.log(square.height);
