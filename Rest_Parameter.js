7ccaae1b35f02a569e139d6c65abebf51a3a4c98
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-09 19:19:34 +0300

    Create Rest_Parameter.js

---
+++ Rest_Parameter.js
@@ -0,0 +1,12 @@
+function containsAll(arr, ...nums) {
+    for (let num of nums) {
+        if (arr.indexOf(num) === -1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+let x = [2, 4, 6, 7];
+console.log(containsAll(x, 2, 4, 7));
+console.log(containsAll(x, 6, 4, 9));
