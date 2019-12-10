0580a7167de23a5916c960712caf64d26b835599
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-10 19:58:40 +0300

    Create Generator_functions_and_Symbol.iterators.js
    The example shows a useful case of how we can use generator functions and Symbol.iterators together.

---
+++ Generator_functions_and_Symbol.iterators.js
@@ -0,0 +1,16 @@
+const arr = ['0', '1', '4', 'a', '9', 'c', '16'];
+const my_obj = {
+  [Symbol.iterator]: function*() {
+    for(let index of arr) {
+      yield `${index}`;
+    }
+  }
+};
+
+const all = [...my_obj] /* Here you can replace the '[...my_obj]' with 'arr'. */
+  .map(i => parseInt(i, 10))
+  .map(Math.sqrt)
+  .filter((i) => i < 5) /* try changing the value of 5 to 4 see what happens.*/
+  .reduce((i, d) => i + d); /* comment this line while you are changing the value of the line above */
+
+console.log(all);
