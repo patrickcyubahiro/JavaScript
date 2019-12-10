d754f169fd705808bc09ebf0326851a3712f6eb5
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-10 17:27:03 +0300

    Create Class_Methods_In_ES6.js

---
+++ Class_Methods_In_ES6.js
@@ -0,0 +1,18 @@
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  
+  get area() {
+    return this.calcArea();
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const square = new Rectangle(5, 5);
+
+console.log(square.area); // 25
