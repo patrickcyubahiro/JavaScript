60d8e9e5350ec0f00d3516f3ebd20e11bec1d668
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-10 17:33:24 +0300

    Create Static_Method.js

---
+++ Static_Method.js
@@ -0,0 +1,18 @@
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.hypot(dx, dy);
+  }
+}
+
+const p1 = new Point(7, 2);
+const p2 = new Point(3, 8);
+
+console.log(Point.distance(p1, p2));
