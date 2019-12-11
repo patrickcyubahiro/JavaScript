15f5107c9da93e7d24f0fd598c097ad8c13d96fd
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2019-12-11 10:28:16 +0300

    Create Searching_Strings.js

---
+++ Searching_Strings.js
@@ -0,0 +1,5 @@
+console.log("SoloLearn".startsWith("Solo", 0)); // true
+console.log("SoloLearn".endsWith("Solo", 4)); // true
+console.log("SoloLearn".includes("loLe")); // true
+console.log("SoloLearn".includes("olo", 1)); // true
+console.log("SoloLearn".includes("olo", 2)); // false
